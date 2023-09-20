258a7c507f9d0b8a3242add099038eced82c4446
David Newhall II <david@sleepers.pro>
2023-09-19 23:24:55 -0700

    Allow a dns name in allowed_ips

--- upstreams.go
+++ upstreams.go
@@ -5,10 +5,23 @@ import (
 	"net"
 	"net/http"
 	"strings"
+	"time"
 )
 
+const dnsRefreshInterval = 3 * time.Minute
+
 func (c *Config) HandleAll(resp http.ResponseWriter, _ *http.Request) {
-	http.Error(resp, "", http.StatusUnauthorized)
+	if c.RedirectURL == "" {
+		resp.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	resp.Header().Add("Location", c.RedirectURL)
+	resp.WriteHeader(http.StatusFound)
+}
+
+func (c *Config) HandleOK(resp http.ResponseWriter, _ *http.Request) {
+	http.Error(resp, "OK", http.StatusOK)
 }
 
 func (c *Config) ValidateUpstream(next http.Handler) http.Handler {
@@ -23,54 +36,62 @@ func (c *Config) ValidateUpstream(next http.Handler) http.Handler {
 
 // AllowedIPs determines who make can requests.
 type AllowedIPs struct {
-	Input []string
-	Nets  []*net.IPNet
+	askIP chan string
+	allow chan bool
+	input []string
+	nets  []*net.IPNet
 }
 
-var _ = fmt.Stringer(AllowedIPs{})
+var _ = fmt.Stringer(&AllowedIPs{})
 
 // String turns a list of allowedIPs into a printable masterpiece.
-func (n AllowedIPs) String() string {
-	if len(n.Nets) < 1 {
+func (n *AllowedIPs) String() string {
+	if len(n.nets) < 1 {
 		return "(none)"
 	}
 
 	output := ""
 
-	for i := range n.Nets {
+	for idx := range n.nets {
 		if output != "" {
 			output += ", "
 		}
 
-		output += n.Nets[i].String()
+		if n.nets[idx] != nil {
+			output += n.nets[idx].String() + " (input: " + n.input[idx] + ")"
+		} else {
+			output += n.input[idx] + " (ignored)"
+		}
 	}
 
 	return output
 }
 
 // Contains returns true if an IP is allowed.
-func (n AllowedIPs) Contains(ip string) bool {
-	ip = strings.Trim(ip[:strings.LastIndex(ip, ":")], "[]")
-
-	for i := range n.Nets {
-		if n.Nets[i].Contains(net.ParseIP(ip)) {
-			return true
-		}
-	}
-
-	return false
+func (n *AllowedIPs) Contains(ip string) bool {
+	n.askIP <- strings.Trim(ip[:strings.LastIndex(ip, ":")], "[]")
+	return <-n.allow
 }
 
 // MakeIPs turns a list of CIDR strings (or plain IPs) into a list of net.IPNet.
 // This "allowed" list is later used to check incoming IPs from web requests.
-func MakeIPs(upstreams []string) AllowedIPs {
-	allowed := AllowedIPs{
-		Input: make([]string, len(upstreams)),
-		Nets:  []*net.IPNet{},
+func MakeIPs(upstreams []string) *AllowedIPs {
+	allowed := &AllowedIPs{
+		askIP: make(chan string),
+		allow: make(chan bool),
+		input: make([]string, len(upstreams)),
+		nets:  make([]*net.IPNet, len(upstreams)),
 	}
+	allowed.parseAndLookup(upstreams)
 
+	go allowed.watch()
+
+	return allowed
+}
+
+func (n *AllowedIPs) parseAndLookup(upstreams []string) {
 	for idx, ipAddr := range upstreams {
-		allowed.Input[idx] = ipAddr
+		n.input[idx] = ipAddr
 
 		if !strings.Contains(ipAddr, "/") {
 			if strings.Contains(ipAddr, ":") {
@@ -80,10 +101,36 @@ func MakeIPs(upstreams []string) AllowedIPs {
 			}
 		}
 
-		if _, i, err := net.ParseCIDR(ipAddr); err == nil {
-			allowed.Nets = append(allowed.Nets, i)
+		if _, ipnet, err := net.ParseCIDR(ipAddr); err == nil {
+			n.nets[idx] = ipnet
+			continue
+		}
+
+		iplist, err := net.LookupHost(n.input[idx])
+		if err == nil && len(iplist) > 0 {
+			if _, ipnet, err := net.ParseCIDR(iplist[0] + "/32"); err == nil {
+				n.nets[idx] = ipnet
+				continue
+			}
 		}
 	}
+}
 
-	return allowed
+func (n *AllowedIPs) watch() {
+	ticker := time.NewTicker(dnsRefreshInterval)
+	for {
+		select {
+		case <-ticker.C:
+			n.parseAndLookup(n.input) // update input w/ input.
+		case askIP := <-n.askIP:
+			for i := range n.nets {
+				if n.nets[i] != nil && n.nets[i].Contains(net.ParseIP(askIP)) {
+					n.allow <- true
+					continue
+				}
+			}
+
+			n.allow <- false
+		}
+	}
 }
